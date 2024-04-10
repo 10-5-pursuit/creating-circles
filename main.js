8fe84ca4f8805b457bb0b888d745e316de852d97
Erimed33 <erikamedina@pursuit.org>
2024-04-10 12:56:02 -0400

    circles

---
+++ main.js
@@ -0,0 +1,17 @@
+const genButton = document.getElementById('button')
+
+genButton.addEventListener('click', generateCircle => {
+    function generateCircle () {
+        // create element
+        //get element and append new element
+        const circle = document.querySelector('div');
+        const  = createElement
+        }
+
+})
+
+// get element by Id 
+//function to generate circles
+
+
+
